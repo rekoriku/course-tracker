9a1166ccad5605aa9363864353903fc9d39dd064
rekoriku <rerikuju@gmail.com>
2019-10-17 14:36:16 +0300

    removing unnecessary comments

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../course';
 import { CourseService } from '../course.service'
-import { HttpService } from 'src/app/http.service';
 
 
 @Component({
@@ -11,30 +10,17 @@ import { HttpService } from 'src/app/http.service';
 })
 export class CourseComponent implements OnInit {
   courses: Course;
-  info : any;
 
-  constructor(private courseService: CourseService, private rest : HttpService) { }
+  constructor(private courseService: CourseService) { }
 
   ngOnInit() {
-    //this.courses = this.courseService.getCourses();
-    //console.log(this.courseService.getCourses());
     this.getCourses();
-    //console.log(val)
-    //this.rest.get('http://34.73.236.56:4000/api/assignments').subscribe(retval => {
-      //console.log(retval);
-     // this.info = retval;
-      //console.log(this.info)
-      //console.log(this.info.Competition.Results);
-   // })
-    //console.log(this.courseService.getCourses());
   }
 
   getCourses(): void {
     this.courseService.getCourses()
     .subscribe(retval => {
-      this.courses = Object.values(retval)[0]
-      console.log(this.courses)
-      //console.log(Object.values(retval))
+      this.courses = Object.values(retval)[0] //get array of objects
     });
   }
 
